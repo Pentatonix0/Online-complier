4e1468c264b4fee9ce29e2a54973121eaac7db3d
Pentatonix0 <Al0250443@yandex.ru>
2025-02-04 18:17:01 +0300

    Update

--- backend/judge0API.py
+++ backend/judge0API.py
@@ -5,21 +5,7 @@ import requests
 
 class Judge0API:
 
-    # url = "https://judge0-ce.p.rapidapi.com"
-
-    # @classmethod
-    # def createSubmission(cls, body):
-    #     headers = {'Content-Type': 'application/json',
-    #                "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-    #                "x-rapidapi-key": "ebeb36eac7msh97c8cdc4a93ec05p173108jsncb232ec76b9a"}
-
-    #     response = requests.post(
-    #         f"{cls.url}/submissions/?base64_encoded=false&wait=false&fields=*", json=body, headers=headers)
-    #     print(response.status_code, response.text)
-    #     print(response.json())
-    #     return response.json()
-    
-    url = "http://92.51.45.95:2358"
+    url = "http://147.45.231.194:2358"
 
     @classmethod
     def createSubmission(cls, body):
@@ -27,8 +13,6 @@ class Judge0API:
 
         response = requests.post(
             f"{cls.url}/submissions/?base64_encoded=true&wait=false&fields=*", json=body, headers=headers)
-        print(response.status_code)
-        print(response.json())
         return response.json()
 
     @classmethod
@@ -36,7 +20,5 @@ class Judge0API:
         headers = {}
         response = requests.get(
             f"{cls.url}/submissions/{token}?base64_encoded=true&wait=false&fields=*", headers=headers)
-        print(response.status_code)
-        print(response.text)
 
         return response.json()
